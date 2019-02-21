ee1fcc082ffae18694e4ae475b246876e33a9095
Saxon Cameron <sacameron@deloitte.com.au>
2019-02-21 21:12:12 +0900

    [FEATURE] Dev server working with React and library import

--- dev/rollup.config.js
+++ dev/rollup.config.js
@@ -5,57 +5,66 @@ import sass from 'node-sass';
 import postcss from 'rollup-plugin-postcss';
 import pcssPresetEnv from 'postcss-preset-env';
 import autoprefixer from 'autoprefixer';
-import clean from 'rollup-plugin-delete';
 import html from 'rollup-plugin-generate-html-template';
 import serve from 'rollup-plugin-serve';
 import livereload from 'rollup-plugin-livereload';
-// import alias from 'rollup-plugin-import-alias';
+import replace from 'rollup-plugin-replace';
 
 
 export default {
 	input: './src/index.js',
-	external: ['react'],
 	output: {
 		file: './dist/index.js',
-		format: 'cjs',
-		globals: { react: 'React' },
+		format: 'umd',
+		sourcemap: 'inline',
 	},
 	plugins: [
-		clean({ targets: './dist' }),
-		babel({ sourceMap: true }),
 		resolve(),
-		commonjs({ sourceMap: true }),
-		// postcss({
-		// 	inject: true,
-		// 	sourceMap: true,
-		// 	exclude: 'node_modules/**',
-		// 	preprocessor: (content, id) => (
-		// 		new Promise(resolve => {
-		// 			const result = sass.renderSync({ file: id });
-		// 			resolve({ code: result.css.toString() });
-		// 		})
-		// 	),
-		// 	plugins: [
-		// 		pcssPresetEnv(),
-		// 		autoprefixer(),
-		// 	],
-		// }),
+		babel({
+			compact: true,
+			sourceMap: true,
+		}),
+		commonjs({
+			sourceMap: true,
+			include: [
+				'node_modules/**',
+			],
+			exclude: [
+				'node_modules/process-es6/**',
+			],
+			namedExports: {
+				'node_modules/react/index.js': ['Children', 'Component', 'PropTypes', 'createElement'],
+				'node_modules/react-dom/index.js': ['render'],
+			},
+		}),
+		postcss({
+			inject: true,
+			sourceMap: true,
+			exclude: 'node_modules/**',
+			preprocessor: (content, id) => (
+				new Promise(resolve => {
+					const result = sass.renderSync({ file: id });
+					resolve({ code: result.css.toString() });
+				})
+			),
+			plugins: [
+				pcssPresetEnv(),
+				autoprefixer(),
+			],
+		}),
 		html({
 			template: './index.html',
 			target: './dist/index.html',
 		}),
 		serve({
-			// open: true,
+			open: true,
 			contentBase: './dist',
 			host: 'localhost',
 			port: '9000',
 		}),
 		livereload('./dist'),
-		// alias({
-		// 	paths: {
-		// 		'dd-breakpoint-container': '../lib',
-		// 	},
-		// 	extensions: ['js', 'scss'],
-		// }),
+		replace({
+			'process.env.NODE_ENV': JSON.stringify('production'),
+		}),
 	],
 };
--- dev/src/index.js
+++ dev/src/index.js
@@ -1,18 +1,16 @@
-// import React from 'react';
-// import ReactDOM from 'react-dom';
-// // import { BreakpointContainer } from 'dd-breakpoint-container';
+import React from 'react';
+import ReactDOM from 'react-dom';
+// import { BreakpointContainer } from 'dd-breakpoint-container';
 
-// class App extends React.Component {
-// 	render() {
-// 		return (
-// 			<p>Lorem ipsum</p>
-// 		);
-// 	}
-// }
+class App extends React.Component {
+	render() {
+		return (
+			<p>Lorem ipsum</p>
+		);
+	}
+}
 
-// ReactDOM.render(
-// 	<App/>,
-// 	window.document.getElementById('app'),
-// );
-
-console.log('!!');
+ReactDOM.render(
+	<App/>,
+	window.document.getElementById('app'),
+);
