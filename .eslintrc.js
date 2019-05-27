fb9b81dbab13dd219ca9942697003e1bde253c1b
Saxon Cameron <sacameron@deloitte.com.au>
2019-05-27 20:49:50 +1000

    [CHORE] Added Prettier, associated plugins, and an amended eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,8 +7,13 @@ module.exports = {
 		},
 	},
 	extends: [
-		'@deloitte-digital-au/eslint-config',
-		'plugin:react/recommended',
+		// Base
+		'@deloitte-digital-au/eslint-config-react',
+		// Imports
+		'plugin:import/errors',
+		'plugin:import/warnings',
+		// Prettier
+		'plugin:prettier/recommended',
 	],
 	env: {
 		browser: true,
@@ -16,21 +21,43 @@ module.exports = {
 		es6: true,
 	},
 	parser: 'babel-eslint',
-	plugins: ['react'],
+	plugins: ['import', 'jsdoc', 'react', 'prettier'],
 	rules: {
-		'react/jsx-uses-vars': 1,
-		'object-curly-spacing': ['error', 'always'],
-		'react/jsx-curly-spacing': [
-			'error',
-			{
-				when: 'never',
-				children: {
-					when: 'always',
-				},
-			},
-		],
-		'react/no-array-index-key': 1,
-		'react/require-default-props': 1,
-		'react/jsx-tag-spacing': ['error', { 'beforeSelfClosing': 'never' }],
+		// Mark Prettier syntax errors
+		'prettier/prettier': 'error',
+
+		// Enforce React.Fragment </> shorthand
+		'react/jsx-fragments': 'error',
+
+		// Max one prop per line; improved readability and diff
+		// Commented out in hope of potential Prettier feature integration
+		// Ref: https://github.com/prettier/prettier/issues/5501
+		// 'react/jsx-max-props-per-line': [1, { when: 'always' }],
+
+		// JSDoc settings
+		// Config docs: https://www.npmjs.com/package/eslint-plugin-jsdoc
+		'jsdoc/check-alignment': 1,
+		'jsdoc/check-examples': 1,
+		'jsdoc/check-indentation': 1,
+		'jsdoc/check-param-names': 1,
+		'jsdoc/check-syntax': 1,
+		'jsdoc/check-tag-names': 1,
+		'jsdoc/check-types': 1,
+		'jsdoc/newline-after-description': 1,
+		'jsdoc/no-undefined-types': 1,
+		// // "jsdoc/require-description": 1,
+		'jsdoc/require-description-complete-sentence': 1,
+		// // "jsdoc/require-example": 1,
+		'jsdoc/require-hyphen-before-param-description': 1,
+		'jsdoc/require-jsdoc': 1,
+		'jsdoc/require-param': 1,
+		// // "jsdoc/require-param-description": 1,
+		'jsdoc/require-param-name': 1,
+		'jsdoc/require-param-type': 1,
+		'jsdoc/require-returns': 1,
+		'jsdoc/require-returns-check': 1,
+		// // "jsdoc/require-returns-description": 1,
+		'jsdoc/require-returns-type': 1,
+		'jsdoc/valid-types': 1,
 	},
 };
---
+++ .prettierrc.js
@@ -0,0 +1,17 @@
+module.exports = {
+	// Note tab width purposefully not configured,
+	// to allow developer preference in their IDE
+	useTabs: true,
+
+	// { foo: bar } as opposed to {foo:bar}, for readability
+	bracketSpacing: true,
+
+	// Single quotes for strings, except in JSX
+	singleQuote: true,
+	quoteProps: "consistent",
+	jsxSingleQuote: false,
+	jsxBracketSameLine: false,
+
+	// Trailing commas for improved diff
+	trailingComma: "all"
+};
