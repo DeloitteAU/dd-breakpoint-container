1925a4e5af7ced8b6eb661d0ff9e7b8da1f1526d
Saxon Cameron <sacameron@deloitte.com.au>
2019-09-19 22:49:55 +1000

    [CHORE] Tweaks to dev folder index.js entry

--- dev/src/index.js
+++ dev/src/index.js
@@ -2,18 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserContainer } from 'dd-breakpoint-container';
 
-// class App extends React.Component {
-// 	render() {
-// 		return (
-// 				<BreakpointContainer debug={true}>
+class App extends React.Component {
+	render() {
+		return <BrowserContainer debug />;
+	}
+}
 
-// 				</BreakpointContainer>
-// 			</div>
-// 		);
-// 	}
-// }
-
-ReactDOM.render(
-	<BrowserContainer debug />,
-	window.document.getElementById('app'),
-);
+ReactDOM.render(<App />, window.document.getElementById('app'));
