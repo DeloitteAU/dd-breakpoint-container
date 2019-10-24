8345f146345c06e2110eeb00f7092276deacd5b2
Saxon Cameron <sacameron@deloitte.com.au>
2019-10-24 16:46:01 +1100

    [DEV] Added some more misc PoC examples to sandbox

--- sandbox/src/App.tsx
+++ sandbox/src/App.tsx
@@ -13,24 +13,46 @@ const App: React.FC = () => {
 			<div className="App">
 				<p>Lorem ipsum browser</p>
 
-				<div style={{ width: '75%', padding: '20px' }}>
+				<div style={{ width: '85%', padding: '20px' }}>
 					<BreakpointContainer className="test-bpc" debug>
 						<div style={{ padding: '20px' }}>
 							<p>Lorem ipsum container</p>
 						</div>
 
 						<Breakpoint q="m">
-							<p>Conditional render at bpc 'l'+</p>
+							<p>Conditional render at bpc 'm'+</p>
 						</Breakpoint>
 
 						<Breakpoint identifier="browser" q="m">
 							<p>Conditional render at browser 'm'+</p>
 						</Breakpoint>
 					</BreakpointContainer>
+				</div>
 
-					<Breakpoint q="m">
-						<p>Conditional render at browser 'm'+</p>
-					</Breakpoint>
+				<div style={{ width: '65%', padding: '20px' }}>
+					<BreakpointContainer identifier="container-1" debug>
+						<div style={{ width: '85%', padding: '20px' }}>
+							<BreakpointContainer identifier="container-2" debug>
+								<div style={{ padding: '20px' }}>
+									<p>Lorem ipsum container</p>
+								</div>
+
+								<Breakpoint q="s">
+									<p>Conditional render at bpc 's'+</p>
+								</Breakpoint>
+								<Breakpoint q="s" identifier="container-1">
+									<p>Conditional render at ctnr 1 's'+</p>
+								</Breakpoint>
+								<Breakpoint q="s" identifier="browser">
+									<p>Conditional render at browser 's'+</p>
+								</Breakpoint>
+							</BreakpointContainer>
+
+							<Breakpoint q="m">
+								<p>Conditional render at browser 'm'+</p>
+							</Breakpoint>
+						</div>
+					</BreakpointContainer>
 				</div>
 			</div>
 		</BrowserContainer>
